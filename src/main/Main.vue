d6d68c83756449fee30ea037cb791012e6eb0825
emily reno <emilyreno@emilys-MacBook-Pro.local>
2019-04-22 14:05:40 -0400

    hosting to netlify

---
+++ src/main/Main.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="wrapper">
+    <h1>This works!</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Main',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  h1 {
+    color: hotpink;
+  }
+</style>
